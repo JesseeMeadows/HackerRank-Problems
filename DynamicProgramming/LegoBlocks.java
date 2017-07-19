c1c03557425337c44a7d53bee5180e104b2bbb58
jesseemeadows <jesseemeadows@gmail.com>
2017-07-19 04:30:48 -0400

    - Add "Lego Blocks" skeleton
     https://www.hackerrank.com/challenges/lego-blocks/problem

---
+++ DynamicProgramming/LegoBlocks.java
@@ -0,0 +1,19 @@
+/*	
+ * Blocks Range from 1 - 4 width
+ * 
+ * Restrictions:
+ * 	<> " The wall you build should be one solid structure. 
+ * 		A solid structure can be interpreted:
+ * 		You cannot make a vertical cut from top to 
+ * 		bottom without cutting one or more lego blocks. "
+ * 
+ * 		-> Otherwords, each column contains 1 blocks that's wider than 1
+ */
+public class LegoBlocks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
