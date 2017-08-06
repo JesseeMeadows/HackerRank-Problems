604912af0a7ae008935ac47e17e4f61b75265e1f
jesseemeadows <jesseemeadows@gmail.com>
2017-08-06 07:22:40 -0400

    update "KnightL on a Chessboard"  Solution

--- Search/KnightLOnAChessBoard.java
+++ Search/KnightLOnAChessBoard.java
@@ -1,37 +1,122 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
 public class KnightLOnAChessBoard {
 
-	// Thoughts: we can use an algorithm similar to prims
+	// Thoughts: we can use an algorithm similar to prims on each
+	// KnightL combination
 	// Basically: 
 	//	    Use a graph G consisting of all possible positions
 	//		perform a BFS from starting position (0,0) to all
 	//		reachable vertices, marking unvisited nodes as visited
 	//      and the amount of turns it took to find
-	//		
-	//		This pre-computation takes up to O(N^2) visits (a,b) = (1,1)
-	//		however, ALL destinations lookups are O(1) after pre-computation
-	//      is finished
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
 		
-//		Create NxN positions graph "visited" to track if a position has been visited
-//		Create NxN graph "distTo" to track amount of moves to each position, and
-//		initialize all positions to -1
+		int[][] distTo = new int[N-1][N-1];
+		
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < N; j++) {
+				if (distTo[i - 1][j - 1] == 0) {
+					int movesToEnd = knightL(i,j, N);
+					distTo[i - 1][j - 1] = movesToEnd;
+					distTo[j - 1][i - 1] = movesToEnd;
+				}
+			}
+		}
+
+
+		//		Print each position in distTo
+		for (int r = 0; r < N - 1; r++) {
+			for (int c = 0; c < N - 1; c++) {
+				System.out.print(distTo[r][c] + " ");
+			}
+			System.out.println();
+		}
+	
+
+	}
+	
+	public static int knightL(int a, int b, int N) {
+//		Create NxN positions matrix "visited" to track if a position has been visited
+		boolean[][] visited = new boolean[N][N];
+		
 //		track amount of moves via "moves"
+		int moves = 0;
+		
 //		Create array "positions" to track newly visited nodes
 //		Add our starting position (0,0) to "positions"
-//		while "positions" isn't empty:
-//			create array "discovered" to hold all newly reachable nodes 
-//			For each p in "positions"
-//				if p isn't visited
-//					mark p visited
-//					record "distTo" of p
+		Deque<Integer> s = new ArrayDeque<Integer>();
+		s.push(0);
+		s.push(0);
+		while (!s.isEmpty()) {
+//			create array "discovered" to hold all newly reachable nodes 	
+			Deque<Integer> discovered = new ArrayDeque<Integer>();
+			
+//			For each p in "positions"		
+			while (!s.isEmpty()) {
+				int x = s.pop();
+				int y = s.pop();
+				
+				if (x == N - 1 && y == N - 1) return moves;
+				
+				if (visited[y][x] == false) {
+					visited[y][x] = true;
+					
 //					add unvisited unreachable nodes from p to "discovered"
-//			replace "positions" with "discovered"
-//		Print each position in distTo
-//		
-//		
+					if (x - a >= 0 && y - b >= 0 && !visited[y - b][x - a]) {
+						discovered.push(y - b);
+						discovered.push(x - a);
+					}
+					
+					if (x - b >= 0 && y - a >= 0 && !visited[y - a][x - b]) {
+						discovered.push(y - a);
+						discovered.push(x - b);
+					}
+					
+					if (x + a < N && y + b < N && !visited[y + b][x + a]) {
+						discovered.push(y + b);
+						discovered.push(x + a);
+					}
+					
+					if (x + b < N && y + a < N && !visited[y + a][x + b]) {
+						discovered.push(y + a);
+						discovered.push(x + b);
+					}
+					
+					if (x + a < N && y - b >= 0 && !visited[y - b][x + a]) {
+						discovered.push(y - b);
+						discovered.push(x + a);
+					}
+					
+					if (x + b < N && y - a >= 0 && !visited[y - a][x + b]) {
+						discovered.push(y - a);
+						discovered.push(x + b);
+					}
+					
+					if (x - a >= 0 && y + b < N && !visited[y + b][x - a]) {
+						discovered.push(y + b);
+						discovered.push(x - a);
+					}
+					
+					if (x - b >= 0 && y + a < N && !visited[y + a][x - b]) {
+						discovered.push(y + a);
+						discovered.push(x - b);
+					}
+					
+	
+				}
+		}
 
+//			replace "positions" with "discovered"
+		s = discovered;
+		moves++;
+		
+		}
+		return -1;
 	}
 
 }
